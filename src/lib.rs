0cf14cabf04d3b08aeb7dd6a9f869285014d2409
Justinas Stankevicius <justinas@justinas.me>
2016-02-01 21:51:58 +0200

    npgettext

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,16 @@
 use std::collections::HashMap;
 use std::ops::Deref;
 
+/// Returns the number of the appropriate plural form
+/// for the given count `n` of objects for germanic languages.
+fn plural_form(n: usize) -> usize {
+    if n != 1 {
+        1
+    } else {
+        0
+    }
+}
+
 /// Catalog represents a set of translation strings
 /// parsed out of one MO file.
 #[derive(Debug)]
@@ -43,11 +53,7 @@ impl Catalog {
     ///
     /// Currently, the only supported plural formula is `n != 1`.
     pub fn ngettext<'a>(&'a self, msg_id: &'a str, msg_id_plural: &'a str, n: usize) -> &'a str {
-        let form_no = if n != 1 {
-            1
-        } else {
-            0
-        };
+        let form_no = plural_form(n);
 
         match self.strings.get(&msg_id.to_owned()) {
             Some(msg) => {
@@ -66,6 +72,32 @@ impl Catalog {
     pub fn pgettext<'a>(&'a self, msg_context: &'a str, msg_id: &'a str) -> &'a str {
         let key = [msg_context, msg_id].join("\x04");
         self.strings.get(&key).and_then(|msg| msg.get_translated(0)).unwrap_or(msg_id)
+    }
+
+    /// Returns the plural translation of `msg_id`
+    /// in the context `msg_context`
+    /// with the correct plural form for the number `n` of objects.
+    /// Returns msg_id if a translation does not exist and `n == 1`,
+    /// msg_id_plural otherwise.
+    ///
+    /// Currently, the only supported plural formula is `n != 1`.
+    // TODO: DRY ngettext/npgettext
+    pub fn npgettext<'a>(&'a self,
+                         msg_context: &'a str,
+                         msg_id: &'a str,
+                         msg_id_plural: &'a str,
+                         n: usize)
+                         -> &'a str {
+        let key = [msg_context, msg_id].join("\x04");
+        let form_no = plural_form(n);
+        match self.strings.get(&key) {
+            Some(msg) => {
+                msg.get_translated(form_no).unwrap_or_else(|| [msg_id, msg_id_plural][form_no])
+            }
+            None if form_no == 0 => msg_id,
+            None if form_no == 1 => msg_id_plural,
+            _ => unreachable!(),
+        }
     }
 }
 
@@ -135,4 +167,21 @@ fn catalog_pgettext() {
     cat.insert(Message::new("Text", Some("unit test"), vec!["Tekstas"]));
     assert_eq!(cat.pgettext("unit test", "Text"), "Tekstas");
     assert_eq!(cat.pgettext("integration test", "Text"), "Text");
+}
+
+#[test]
+fn catalog_npgettext() {
+    let mut cat = Catalog::new();
+    cat.insert(Message::new("Text", Some("unit test"), vec!["Tekstas", "Tekstai"]));
+
+    assert_eq!(cat.npgettext("unit test", "Text", "Texts", 1), "Tekstas");
+    assert_eq!(cat.npgettext("unit test", "Text", "Texts", 0), "Tekstai");
+    assert_eq!(cat.npgettext("unit test", "Text", "Texts", 2), "Tekstai");
+
+    assert_eq!(cat.npgettext("integration test", "Text", "Texts", 1),
+               "Text");
+    assert_eq!(cat.npgettext("integration test", "Text", "Texts", 0),
+               "Texts");
+    assert_eq!(cat.npgettext("integration test", "Text", "Texts", 2),
+               "Texts");
 }
